966740fa7089e1c0d0c0922e6ed00bc16f1512e5
Abdo <7afizooo@gmail.com>
2023-10-23 17:20:28 +0300

    create CustomIcon

---
+++ lib/views/widgets/custom_search_icon.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CustomSearchIcon extends StatelessWidget {
+  const CustomSearchIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(8),
+      width: 47,
+      height: 47,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        color: Colors.white.withOpacity(.06),
+      ),
+      child: const Icon(Icons.search, color: Colors.white),
+    );
+  }
+}

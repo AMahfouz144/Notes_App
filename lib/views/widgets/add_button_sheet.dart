441b1652f7b05e6814f363e1199ceb11c8cf5d25
Abdo <7afizooo@gmail.com>
2023-11-05 10:05:16 +0200

    create button sheet

---
+++ lib/views/widgets/add_button_sheet.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:nots_app/views/widgets/custom_text_field.dart';
+
+class CustomAddButtonSheet extends StatelessWidget {
+  const CustomAddButtonSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 30,
+          ),
+          CustomTextField(
+            maxLength: 5,
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          CustomTextField(
+            maxLength: 8,
+          ),
+        ],
+      ),
+    );
+  }
+}
